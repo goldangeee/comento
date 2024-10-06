d1cc3c462e0e67e55e886013dd15efbd8bc98c3d
goldangeee <goldangeee@proton.me>
2024-10-06 19:38:42 +0900

    3주차 django 개발 완료

--- product/views.py
+++ product/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import MainContent
+from django.shortcuts import get_object_or_404
+
 def index(request):
     # return HttpResponse("Hello World")
-
     content_list = MainContent.objects.order_by('-pub_date')
-    context = {'content_list0': content_list[0],'content_list1': content_list[1]}
+    context = {'content_list': content_list}
     return render(request, 'product/content_list.html',context)
+
+def detail(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list':content_list}
+    return render(request,'product/content_detail.html',context)
